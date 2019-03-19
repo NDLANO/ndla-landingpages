cc80d1391af8ad21c2955a9cbb447910f32b1c6e
Robert Fohlin <robert@fohlin.io>
2019-03-19 16:41:49 +0000

    Added more styling and functions for frontpage

--- wp-content/themes/ndla/front-page.php
+++ wp-content/themes/ndla/front-page.php
@@ -13,4 +13,25 @@
     <?php endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
   <?php endif; ?>
+  <?php
+    $args = array(
+      'category' => 2,
+      'post_type' => 'post',
+      'numberposts' => 4
+    );
+    $frontpagePosts = get_posts($args);
+    // print '<pre>';
+    // print_r($frontpagePosts);
+    // print '</pre>';
+  ?>
+  <div class="ndla-frontpage__latest">
+    <?php foreach($frontpagePosts as $post) : setup_postdata($post) ?>
+    <div class="ndla-frontpage__post">
+      <div class="ndla-frontpage__post__image"></div>
+      <h2 class="ndla-frontpage__post__title">
+        <?php echo $post->post_title; ?>
+      </h2>
+    </div>
+    <?php endforeach ?>
+  </div>
 </div>
