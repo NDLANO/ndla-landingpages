0969406f786a48d2cf7f184a433c0de333aeef7a
Robert Fohlin <robert@fohlin.io>
2019-03-26 16:39:31 +0000

    Search and styling

--- wp-content/themes/ndla/index.php
+++ wp-content/themes/ndla/index.php
@@ -11,7 +11,6 @@
     </aside>
     <?php if (have_posts()) : while (have_posts()) : the_post();
       get_template_part( 'template_parts/content/content', 'page' );
-
     ?>
       <!-- <div>
         <h1><?php the_title(); ?></h1>
--- wp-content/themes/ndla/search.php
+++ wp-content/themes/ndla/search.php
@@ -1,4 +1,27 @@
-<?php ?>
-<div>
-  Search goes here
-</div>
+<?php
+  get_header();
+?>
+<section id="primary" class="content-area">
+  <main id="main" class="ndla-main" role="main">
+    <header class="page-header">
+      <?php get_search_form(); ?>
+      <h1 class="page-title">
+        Søkresultat for <?php echo get_search_query(); ?>
+      </h1>
+      <div class="page-description"></div>
+    </header><!-- .page-header -->
+    <?php if ( have_posts() ) : ?>
+      <div class="ndla-search__results">
+        <?php
+  			// Start the Loop.
+  			while ( have_posts() ) :
+  				the_post();
+
+          get_template_part( 'template_parts/content/content', 'excerpt' );
+  				// End the loop.
+  			endwhile;
+        ?>
+      </div>
+    <?php endif; ?>
+  </main>
+</section>
