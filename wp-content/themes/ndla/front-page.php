8ec70432b27f1076b43a3d05d7f5ad15ed1af623
Robert Fohlin <robert@fohlin.io>
2019-03-27 14:47:19 +0000

    Added more settings for featured posts on frontpage

--- wp-content/themes/ndla/front-page.php
+++ wp-content/themes/ndla/front-page.php
@@ -5,6 +5,9 @@
    get_header();
 ?>
 <div class="ndla-frontpage__content">
+  <header class="ndla-welcome">
+    <?php dynamic_sidebar( 'ndla-welcome' ); ?>
+  </header>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div>
       <p><?php the_content(__('(more...)')); ?></p>
@@ -22,12 +25,13 @@
   ?>
   <section class="ndla-frontpage__latest">
     <?php foreach($frontpagePosts as $post) : setup_postdata($post) ?>
-    <div class="ndla-frontpage__post">
+    <div class="ndla-frontpage__post <?php if(get_field('white_title')) { echo 'white'; }?>">
       <a href="<?php echo get_permalink($post->ID) ?>">
         <div class="ndla-frontpage__post__image">
           <?php print the_post_thumbnail( 'medium_large' ); ?>
         </div>
-        <h2 class="ndla-frontpage__post__title">
+        <?php $titlePosition = get_field('titelposition'); ?>
+        <h2 class="ndla-frontpage__post__title <?php echo $titlePosition ?> <?php echo get_field('titelposition_vertikal')?>">
           <?php echo $post->post_title; ?>
         </h2>
       </a>
@@ -51,15 +55,18 @@
       <?php endforeach ?>
     </ul>
     </div> <!-- End of most viewed -->
+    <!-- get shortcut widget -->
+    <?php dynamic_sidebar( 'ndla-hurtigvalg' ); ?>
     <!-- Guides -->
-    <div class="ndla-frontpage__guides">
+    <!-- <div class="ndla-frontpage__guides">
       <h3>Jeg skal...</h3>
       <ul>
         <li class="ndla-frontpage__guide"><a href="#">... ha eksamen</a></li>
         <li class="ndla-frontpage__guide"><a href="#">... levere oppgave</a></li>
         <li class="ndla-frontpage__guide"><a href="#">... holde en presentasjon</a></li>
       </ul>
-    </div>
+
+    </div> -->
     <!-- End of guides -->
   </section>
 </div>
--- wp-content/themes/ndla/functions.php
+++ wp-content/themes/ndla/functions.php
@@ -19,7 +19,7 @@
   add_theme_support( 'responsive-embeds' );
   // Add support for thumbnails
   add_theme_support( 'post-thumbnails' );
-  
+
   /*
    * Set post views count using post meta
    */
@@ -42,7 +42,7 @@
   function ndla_sidebar() {
     register_sidebar(
       array (
-          'name' => __( 'Sidebar Widget', 'ndla-theme' ),
+          'name' => __( 'Sidebar Widget', 'ndla2019' ),
           'id' => 'ndla-sidebar',
           'description' => __( 'Sidemeny', 'ndla-theme' ),
           'before_widget' => '<div class="ndla-sidebar__widget">',
@@ -52,5 +52,52 @@
       )
     );
   }
+  /* Widget for welcome text on frontpage */
+  function register_welcome_widget() {
+    register_sidebar(
+  		array(
+  			'name'          => __( 'Velkomsttekst landingside', 'ndla2019' ),
+  			'id'            => 'ndla-welcome',
+        'class'         => 'ndla-welcome',
+  			'description'   => __( 'Innehold for velkomsttekst på landingsiden.', 'ndla2019' ),
+  			'before_widget' => '<div id="%1$s" class="ndla-welcome__widget %2$s">',
+  			'after_widget'  => '</div>',
+  			'before_title'  => '<h2 class="ndla-welcome__title">',
+  			'after_title'   => '</h2>',
+  		)
+    );
+  }
+  /* Custom shortcut widget for frontpage */
+  function register_featured_posts() {
+    register_sidebar(
+      array(
+        'name'          => __( 'Utvalde inlegg', 'ndla2019' ),
+        'id'            => 'ndla-featured',
+        'description'   => __( 'Innehold for utvalde poster for landingside.', 'ndla2019' ),
+        'before_widget' => '<div id="%1$s" class="widget ndla-frontpage__post %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="ndla-frontpage__post__title">',
+        'after_title'   => '</h2>',
+      )
+    );
+  }
+  /* Custom shortcut widget for frontpage */
+  function register_shortcuts() {
+    register_sidebar(
+  		array(
+  			'name'          => __( 'Hurtigvalg landingside', 'ndla2019' ),
+  			'id'            => 'ndla-hurtigvalg',
+  			'description'   => __( 'Innehold for hurtigvalgs widget.', 'ndla2019' ),
+  			'before_widget' => '<div id="%1$s" class="widget ndla-shortcuts %2$s">',
+  			'after_widget'  => '</div>',
+  			'before_title'  => '<h3 class="widget-title">',
+  			'after_title'   => '</h3>',
+  		)
+    );
+  }
+
   add_action( 'widgets_init', 'ndla_sidebar' );
+  add_action( 'widgets_init', 'register_shortcuts' );
+  add_action( 'widgets_init', 'register_welcome_widget' );
+  add_action( 'widgets_init', 'register_featured_posts' );
 ?>
