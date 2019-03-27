84ab2e395db6f86a772a1982bb9eee956651561b
Robert Fohlin <robert@fohlin.io>
2019-03-27 16:28:25 +0000

    More search page

--- wp-content/themes/ndla/search.php
+++ wp-content/themes/ndla/search.php
@@ -1,27 +1,24 @@
 <?php
   get_header();
 ?>
-<section id="primary" class="content-area">
-  <main id="main" class="ndla-main" role="main">
-    <header class="page-header">
-      <?php get_search_form(); ?>
-      <h1 class="page-title">
-        Søkresultat for <?php echo get_search_query(); ?>
-      </h1>
-      <div class="page-description"></div>
-    </header><!-- .page-header -->
-    <?php if ( have_posts() ) : ?>
-      <div class="ndla-search__results">
-        <?php
-  			// Start the Loop.
-  			while ( have_posts() ) :
-  				the_post();
+<main id="main" class="ndla-main ndla-main--column" role="main">
+  <header class="ndla-search__header">
+    <?php get_search_form(); ?>
+    <span class="search-count"><?php echo $wp_query->found_posts.' treff i NDLA.'; ?></span>
+    <div class="page-description"></div>
+  </header><!-- .page-header -->
+  <?php if ( have_posts() ) : ?>
+    <div class="ndla-search__results">
+      <h1>Du søkte på: <span class="query"><?php echo get_search_query(); ?></span></h1> 
+      <?php
+			// Start the Loop.
+			while ( have_posts() ) :
+				the_post();
 
-          get_template_part( 'template_parts/content/content', 'excerpt' );
-  				// End the loop.
-  			endwhile;
-        ?>
-      </div>
-    <?php endif; ?>
-  </main>
-</section>
+        get_template_part( 'template_parts/content/content', 'excerpt' );
+				// End the loop.
+			endwhile;
+      ?>
+    </div>
+  <?php endif; ?>
+</main>
